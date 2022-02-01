556db7e5b7211647131d6fdfac128a78166bb0a7
HYUNJIN SA <56509933+tkguswls1106@users.noreply.github.com>
2022-02-01 16:16:27 +0900

    Update FizzBuzz.py
    보충설명 추가.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -9,3 +9,7 @@ for i in range(1, 101):
                 print('Buzz')
         else:
                 print(i)
+
+# 위의 긴 코드들을 짧은 코드로 단축시키면
+# for i in range(1, 101):
+#         print('Fizz' * (i%3==0) + 'Buzz' * (i%5==0) or i)
